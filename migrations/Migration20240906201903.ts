dc6f0f614fa4bb137a16f789a6077e5c9468d332
hwisu <hwitticus@gmail.com>
2024-09-07 05:21:14 +0900

    cats and rats:  exmaple

---
+++ migrations/Migration20240906201903.ts
@@ -0,0 +1,14 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20240906201903 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql('create table `rat` (`_id` integer not null primary key autoincrement, `killed_by__id` integer not null, `created_at` datetime not null, constraint `rat_killed_by__id_foreign` foreign key(`killed_by__id`) references `cat`(`_id`) on update cascade);');
+    this.addSql('create index `rat_killed_by__id_index` on `rat` (`killed_by__id`);');
+
+    this.addSql('alter table `cat` drop column `hidden_field`;');
+
+    this.addSql('alter table `cat` add column `created_at` datetime not null;');
+  }
+
+}
--- src/cats/cat.entity.ts
+++ src/cats/cat.entity.ts
@@ -1,12 +1,16 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import {Entity, OneToMany, PrimaryKey, Property} from "@mikro-orm/core";
+import {Rat} from "../rats/rat.entity";
 
 @Entity()
 export class Cat {
     @PrimaryKey({autoincrement: true  })
     _id: number
 
-    @Property({hidden: true})
-    hiddenField = Date.now()
+    @Property()
+    createdAt = new Date();
+
+    @OneToMany(() => Rat, (rat) => rat.killedBy)
+    rats: Rat[] = [];
 
     @Property({ persist: false })
     count?: number;
