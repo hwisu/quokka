6796e75ed64921933001ac91e3a5d2b7798d1425
hwisu <hwitticus@gmail.com>
2024-09-10 17:01:12 +0900

    apply: middlewaress

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import fastifyHelmet from "@fastify/helmet";
+import fastifyRateLimit from "@fastify/rate-limit";
+import fastifyCompress from "@fastify/compress";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -8,6 +11,13 @@ async function bootstrap() {
      new FastifyAdapter(),
     );
   app.enableShutdownHooks();
+  app.enableCors();
+  await app.register(fastifyRateLimit, {
+    max: 100,
+    timeWindow: '1 minute',
+  });
+  await app.register(fastifyHelmet);
+  await app.register(fastifyCompress);
 
 
   await app.listen(3000, "0.0.0.0");
